03f1b57eadddbe9822d7be30ddc9ba745dff23d6
Evaristo <bubbali2002@gmail.com>
2021-07-12 23:45:42 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.querySelector("main#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Eva is the champion";
+document.body.appendChild(newHeader);
